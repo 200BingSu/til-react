0baf5295d573fcdb38c9c0a4e2f2004dce724a2e
github 200BingSu <github 200BingSu@gmail.com>
2024-12-03 16:32:03 +0900

    [CRUD 실습]

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 import Todo from "./todos/Todo";
+import Diary from "./diaries/Diary";
 
 createRoot(document.getElementById("root")).render(
   <>
-    <Todo />
+    <Diary />
   </>,
 );
